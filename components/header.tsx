5cfb6f1c3a1ffe4ffa1f6da59b8ceeae8d05c406
ankitk26 <myselfankit51@gmail.com>
2026-01-28 02:29:58 +0530

    fix header

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,25 @@
 "use client";
 
-// import GitHubIcon from "@material-ui/icons/GitHub";
 import Link from "next/link";
 import { useEditor } from "./app-provider";
 import LanguageMenu from "./language-menu";
 import OrientationMenu from "./orientation-menu";
-import SettingsModal from "./settings-modal";
+// import SettingsModal from "./settings-modal";
+import { IconBrandGithub, IconSettings } from "@tabler/icons-react";
+import { Button } from "./ui/button";
 
-const Header = () => {
+export default function Header() {
 	const { language } = useEditor();
 
 	return (
-		<nav className="flex items-center justify-between px-16 py-3 shadow-md bg-paper">
+		<nav className="flex items-center justify-between px-16 py-2 shadow-md">
 			{/* Logo */}
-			<Link
-				href="/"
-				className="flex text-2xl font-bold tracking-wider text-white items-center gap-3 pointer"
-			>
-				NextPen
+			<Link href="/" className="text-lg font-medium">
+				nextpen
 			</Link>
 
 			{/* Navigation buttons */}
-			<div className="flex items-center justify-center gap-5 text-white">
+			<div className="flex items-center justify-center gap-4">
 				{/* Change the orientation of editors (only for webd view) */}
 				{language === "webd" && <OrientationMenu />}
 
@@ -29,17 +27,19 @@ const Header = () => {
 				<LanguageMenu />
 
 				{/* Opens settings modal */}
-				<SettingsModal />
+				<Link href="/settings">
+					<Button size="icon">
+						<IconSettings />
+					</Button>
+				</Link>
+				{/*<SettingsModal />*/}
 
-				{/*<a
-					href="https://github.com/ankitk26/NextPen"
-					className="flex items-center gap-2 px-4 py-2 rounded-md justify-evenly hover:text-textSecondary focus:outline-none"
-				>
-					<GitHubIcon />
-				</a>*/}
+				<Button size="icon" variant="secondary">
+					<a href="https://github.com/ankitk26/nextpen">
+						<IconBrandGithub className="size-5" />
+					</a>
+				</Button>
 			</div>
 		</nav>
 	);
-};
-
-export default Header;
+}
